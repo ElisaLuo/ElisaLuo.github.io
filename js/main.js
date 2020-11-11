9ed73858febcb0c367b13e3d95b9c5430465132d
ElisaLuo <luolixuan2003@gmail.com>
2020-11-11 15:49:35 -0500

    chem website update

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    // Toggle side bar
+    $('a.sidebar-toggle').on("click", function () {
+        $('#sidebar').sidebar('toggle');
+        console.log("pressed");
+    })
+
+    // Allow image to check out more when hover
+    $('.special.cards .image').dimmer({
+        on: 'hover'
+    });
+})
+
